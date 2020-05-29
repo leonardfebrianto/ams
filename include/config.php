4dd7470825e3625411d001e2c25804e051548b88
Leonard Febrianto <39395986+leonardfebrianto@users.noreply.github.com>
2020-05-29 23:01:30 +0700

    First Commit

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php
+$host     = "127.0.0.1";    // Nama host
+$username = "root";         // Username database
+$password = "";   // Password database
+$database = "ams_native";   // Nama database
